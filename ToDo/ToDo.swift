2688e359168d05f7847c27b1fb0e29acb2ce10f0
soapia <sofia.ongele@ealas.org>
2019-05-23 10:26:23 -0400

    iteration 0

---
+++ ToDo/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDo
+//
+//  Created by Sofia Ongele on 5/23/19.
+//  Copyright © 2019 Sofia Ongele. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
